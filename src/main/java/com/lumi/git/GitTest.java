ec834101f883d775ad856611da576e479bc92abb
dengzhe <zhe.deng-a1712@aqara.com>
2021-08-16 16:13:38 +0800

    2nd commit

--- src/main/java/com/lumi/git/GitTest.java
+++ src/main/java/com/lumi/git/GitTest.java
@@ -6,6 +6,7 @@ package com.lumi.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!");
+        System.out.println("hello git1!");
+        System.out.println("hello git2!");
     }
 }
