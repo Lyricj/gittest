487c304c05046d7bd0b46727dd94eb4663292334
dengzhe <zhe.deng-a1712@aqara.com>
2021-08-16 16:11:56 +0800

    1st commit

---
+++ src/main/java/com/lumi/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.lumi.git;
+
+/**
+ * @author lyricjstart
+ * @create 2021-08-16 16:00
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
